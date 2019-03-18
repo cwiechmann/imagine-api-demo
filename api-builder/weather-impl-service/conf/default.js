be77965e6f55b05c3771f25d13726301655964bb
Chris Wiechmann <cwiechmann@axway.com>
2019-03-18 17:02:38 +0100

    Some more fine-tuning.

--- api-builder/weather-impl-service/conf/default.js
+++ api-builder/weather-impl-service/conf/default.js
@@ -23,7 +23,7 @@ module.exports = {
 	baseurl: 'http://localhost',
 
 	// This is the port the service will be bound to
-	port: 8080,
+	port: 8081,
 
 	// Enabling this property will print out the process.env at startup time
 	printEnvVars: false,
